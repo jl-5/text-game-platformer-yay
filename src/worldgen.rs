f142715b926a1c84e967e3ca90393c1037eed27d
MushroomManToad <mushroommantoad@hotmail.com>
2023-09-12 21:45:37 -0700

    WorldGenPatch

--- src/worldgen.rs
+++ src/worldgen.rs
@@ -1,4 +1,5 @@
 use super::WORLDSIZE;
+use rand::Rng;
 
 // The code used to generate the map.
 pub fn gen_world(world: &mut [[char;WORLDSIZE.0];WORLDSIZE.1]) {
@@ -6,4 +7,7 @@ pub fn gen_world(world: &mut [[char;WORLDSIZE.0];WORLDSIZE.1]) {
     for i in 0..(WORLDSIZE.0) {
         world[WORLDSIZE.1 - 1][i] = 'T';
     }
+
+    // "I can't believe it's not Flagpole! [TM]"
+    
 }
