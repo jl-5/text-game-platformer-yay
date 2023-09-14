65ab1b4865019460c5b0b2b43f3aca9f4ef779f1
MushroomManToad <mushroommantoad@hotmail.com>
2023-09-13 19:59:51 -0700

    BONK

--- src/physicsloop.rs
+++ src/physicsloop.rs
@@ -139,15 +139,17 @@ fn do_gravity(player: &mut Player, game_state: &mut [[char;WORLDSIZE.0];WORLDSIZ
     if player.accel.1 > 0
     {
         let mut snapto: i32 = player.pos.1 as i32 + player.accel.1;
+        let stored_accel = player.accel.1;
         // player's base
-        'LOOP: for j in 1..=player.accel.1 {
+        for j in 1..=stored_accel {
             for i in 0..=2 {
                 let targety = if player.pos.1 + j as usize > WORLDSIZE.1 - 1 {WORLDSIZE.1 - 1} else {player.pos.1 + j as usize};
                 match game_state[targety][i + player.pos.0 - 1] {
                     '#' | 'T' => {
-                        snapto = targety as i32 - 1;
+                        if targety as i32 - 1 < snapto {
+                            snapto = targety as i32 - 1;
+                        }
                         player.set_accel(player.accel.0, 0);
-                        break 'LOOP;
                     }
                     _ => {
 
@@ -160,15 +162,17 @@ fn do_gravity(player: &mut Player, game_state: &mut [[char;WORLDSIZE.0];WORLDSIZ
     // Ascend (Negative Y Velocity)
     else if player.accel.1 < 0 {
         let mut snapto: i32 = player.pos.1 as i32 + player.accel.1;
+        let stored_accel = player.accel.1;
         // player's base
-        'LOOP: for i in 0..=2 {
-            for j in 1..=player.accel.1 {
+        for j in 1..=-stored_accel {
+            for i in 0..=2 {
                 let targety = if player.pos.1 as i32 - j - 2 < 0 {0} else {player.pos.1 - 2 - j as usize};
                 match game_state[targety][i + player.pos.0 - 1] {
                     '#' | 'T' => {
-                        snapto = targety as i32 + 2;
+                        if targety as i32 + 3 > snapto {
+                            snapto = targety as i32 + 3;
+                        }
                         player.set_accel(player.accel.0, 0);
-                        break 'LOOP;
                     }
                     _ => {}
                 }
